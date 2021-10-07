4e010b3e5d23623b51dd03d9a9c1954c5fb1ff03
Valentyn <akuras@ex.ua>
2021-10-07 12:02:08 +0000

    sudo added

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,12 @@ apt_update(){
 }
 remove_old_site(){
 	if [ ! -d /var/www/html/.git ]; then
-           rm -f /var/www/html/index.html
+          sudo rm -f /var/www/html/index.html
 	fi
 }
 clone_website_code(){
 	if [ ! -d /var/www/html/.git ]; then
-	  git clone https://github.com/octocat/Spoon-Knife /var/www/html/
+	  sudo git clone https://github.com/octocat/Spoon-Knife /var/www/html/
 	fi
 }
 
