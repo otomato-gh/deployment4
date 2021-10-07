cd84e4e6f7e59b2de677a44580a0e49014075370
Anton Weiss <anton@otomato.io>
2021-10-07 10:27:02 +0000

    Tasks

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,13 @@ install_package(){
 apt_update(){
 	sudo apt-get update
 }
-
+remove_old_site(){
+	if [ ! -d /var/www/html/.git ]; then
+           rm -f /var/www/html/index.html
+	fi
+}
 clone_website_code(){
-	if ![ -d /var/www/html/.git]; then
+	if [ ! -d /var/www/html/.git ]; then
 	  git clone https://github.com/octocat/Spoon-Knife /var/www/html/
 	fi
 }
@@ -25,5 +29,6 @@ print_title
 print_date
 apt_update
 install_package git
-install_package apache
-
+install_package apache2
+remove_old_site
+clone_website_code
